67d5d34a8831f2a8ad5a63850bbe494d67b3b588
Isaac Hui <isaachui89@gmail.com>
2018-09-27 16:26:06 -0700

    adding DogstatsdOutput

--- logster/outputs/builtin.py
+++ logster/outputs/builtin.py
@@ -1,12 +1,15 @@
 from logster.outputs.stdout import StdoutOutput
+from logster.outputs.dogstatsd import DogstatsdOutput
 from logster.outputs.graphite import GraphiteOutput
 from logster.outputs.ganglia import GangliaOutput
 from logster.outputs.statsd import StatsdOutput
 from logster.outputs.cloudwatch import CloudwatchOutput
 from logster.outputs.nsca import NSCAOutput
 
+
 builtin_output_classes = (
         StdoutOutput,
+        DogstatsdOutput,
         GraphiteOutput,
         GangliaOutput,
         StatsdOutput,
